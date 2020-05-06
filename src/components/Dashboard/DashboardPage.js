b695f889e4b0063f2ffde79df8ae0f8eecea5898
Jose <josepablopt@gmail.com>
2020-05-05 20:23:19 -0500

    added time and call to firestore

--- src/components/Dashboard/DashboardPage.js
+++ src/components/Dashboard/DashboardPage.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
+import moment from "moment";
 
-import EmptyState from "../EmptyState";
+import PropTypes from "prop-types";
 
-function DashboardPage() {
-  return <EmptyState title="Dashboard" />;
+import { withRouter } from "react-router-dom";
+
+import { auth, firestore,  } from "../../firebase";
+
+
+let dateAndTime = moment().format("DD/MM/YYYY HH:mm:ss");
+
+
+class DashboardPage extends Component {
+
+  getClientData = () => {
+    var db = firestore;
+    db.collection("clients").get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+      });
+    });
+  }
+
+  
+  render() {
+    const { user } = this.props;
+    console.log(dateAndTime);
+
+    let currentBalanceCard = (
+      <div>
+        <h1>Current Balance: </h1>
+        <ul>{dateAndTime}</ul>
+      </div>
+    );
+
+    return <div>{currentBalanceCard}</div>;
+  }
+
+  componentDidMount() { 
+    this.getClientData()
+  }
+ 
 }
 
-export default DashboardPage;
+DashboardPage.propTypes = {
+  user: PropTypes.object,
+};
+
+export default withRouter(DashboardPage);
