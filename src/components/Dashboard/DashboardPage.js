1dac1c5cd56c019ff67c9a3501b5d144fb9c4169
Jose <josepablopt@gmail.com>
2020-05-06 01:03:04 -0500

    got dialog box to open

--- src/components/Dashboard/DashboardPage.js
+++ src/components/Dashboard/DashboardPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import moment from "moment";
 
 import PropTypes from "prop-types";
@@ -7,12 +8,60 @@ import { withRouter } from "react-router-dom";
 
 import { auth, firestore,  } from "../../firebase";
 
+import Button from '@material-ui/core/Button';
+
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+
+import AddIcon from '@material-ui/icons/Add';
+
+import { withStyles } from '@material-ui/core/styles';
+
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+});
+
+
+
+
 
 let dateAndTime = moment().format("DD/MM/YYYY HH:mm:ss");
 
 
 class DashboardPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      setOpen: false
+    };
+  }
 
+  handleClickOpen = () => {
+    this.setState({
+      setOpen: true,
+      open:true
+    })
+  };
+  handleClose = () => {
+    this.setState({
+      setOpen:false,
+      open:false
+      
+    })
+  };
+
+ 
   getClientData = () => {
     var db = firestore;
     db.collection("clients").get().then(function(querySnapshot) {
@@ -22,20 +71,90 @@ class DashboardPage extends Component {
       });
     });
   }
-
   
+  addClientClick = () => {
+    var db = firestore;
+    console.log("the button has been pressed adding clitn ")
+    this.handleClickOpen()
+
+  //   db.collection("clients").add({
+  //     FristName: "Tokyo",
+  //     LastName: "Japan",
+  //     timestamp: dateAndTime,
+  //     BillingAddress: { Address: "3000 9th Lane", City: "Anoka", Zipcode: "55421" },
+  //     PhoneNumber: "612-324-432",
+
+  // })
+  // .then(function(docRef) {
+  //     console.log("Document written with ID: ", docRef.id);
+  // })
+  // .catch(function(error) {
+  //     console.error("Error adding document: ", error);
+  // });
+   }
+  
+   
+
   render() {
+  
+   
     const { user } = this.props;
+    const classes = withStyles();
     console.log(dateAndTime);
 
     let currentBalanceCard = (
       <div>
         <h1>Current Balance: </h1>
         <ul>{dateAndTime}</ul>
+        <Button
+        variant="contained"
+        color="default"
+        className={classes.button}
+        startIcon={<AddIcon />}
+        onClick={this.addClientClick}
+        
+      > New Client
+      </Button>
       </div>
     );
 
-    return <div>{currentBalanceCard}</div>;
+    return <div>
+      {currentBalanceCard}
+      <h1>test</h1>
+      <Dialog open={this.state.open } onClose={this.handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={this.handleClose} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+
+
+
+
+
+
+
+    </div>;
   }
 
   componentDidMount() { 
@@ -48,4 +167,4 @@ DashboardPage.propTypes = {
   user: PropTypes.object,
 };
 
-export default withRouter(DashboardPage);
+export default withRouter(withStyles(styles)(DashboardPage));
