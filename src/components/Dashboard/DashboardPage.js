a14c18170438827394ca8568cbca7a785122c40f
Jose <josepablopt@gmail.com>
2020-05-23 12:19:54 -0500

    moving fab to the right 10

--- src/components/Dashboard/DashboardPage.js
+++ src/components/Dashboard/DashboardPage.js
@@ -468,7 +468,7 @@ class DashboardPage extends Component {
         <Fab
           color="primary"
           aria-label="add"
-          style={{ bottom: 75, position: "fixed", right: 2 }}
+          style={{ bottom: 75, position: "fixed", right: 10 }}
           onClick={this.addClientClick}
         >
           <AddIcon />
